a87b2c9a12308af7d326cc31bfe15c50e6e97cc5
Marcogarofalo <marcogarofalo31@gmail.com>
2024-06-07 11:34:44 +0200

    build with sanitize

--- sanitize_build/do_cmake.sh
+++ sanitize_build/do_cmake.sh
@@ -3,9 +3,10 @@ rm -rv CMakeFiles
 
 cmake -DCMAKE_PREFIX_PATH=/home/garofalo/analysis/analysis_program/build_sanitizer/install_dir \
       -DCMAKE_BUILD_TYPE=DEBUG\
-      -DCMAKE_CXX_FLAGS="-g -O2"  \
+      -DCMAKE_CXX_FLAGS="-fopenmp   -pedantic  -g    -lm"  \
       -DWITH_ARB=OFF \
-      -DCMAKE_VERBOSE_MAKEFILE=OFF \
+      -DCMAKE_VERBOSE_MAKEFILE=ON \
+      -DCMAKE_CXX_COMPILER=/usr/bin/g++ \
        \
       ..
 
